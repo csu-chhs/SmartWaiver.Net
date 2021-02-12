1a8cf3a489cadbc32e57bec8cb7f3f806885692e
Derek Stegelman <dstegelman@users.noreply.github.com>
2021-02-12 08:05:39 -0700

    Bumps

--- SmartWaiver.Net.Tests/SmartWaiverTests.cs
+++ SmartWaiver.Net.Tests/SmartWaiverTests.cs
@@ -15,5 +15,17 @@ namespace SmartWaiver.Net.Tests
             Assert.Equal("https://waiver.smartwaiver.com/auto/?auto_waiverid=4fc7d12601941&auto_tag=1234",
                 smartWaiver.GetAutoTaggedWaiverUrl(waiverGUID, autoTag));
         }
+
+        [Fact]
+        public void VerifyAutoTagUpdate()
+        {
+            string waiverGUID = "4fc7d12601941";
+            string autoTag = "1234";
+
+            SmartWaiver smartWaiver = new SmartWaiver("laskdfj");
+
+            Assert.Equal("https://waiver.smartwaiver.com/auto/?auto_waiverid=4fc7d12601941&auto_tag=1234",
+                smartWaiver.Waiver.GetAutoTaggedWaiverUrl(waiverGUID, autoTag));
+        }
     }
 }
